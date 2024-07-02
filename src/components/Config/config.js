c1beef352db2e8e8eaa62cb0965a2c41f77e54b5
Adam29C <adamcatholic29@gmail.com>
2024-07-02 18:35:16 +0530

    change IP

--- src/components/Config/config.js
+++ src/components/Config/config.js
@@ -1,3 +1,4 @@
 // export const base_url = "http://localhost:5000/"
 // export const base_url = "http://51.20.125.209:5000/"
-export const base_url = "http://16.171.241.112:5000/"
+// export const base_url = "http://16.171.241.112:5000/"
+export const base_url = "http://13.51.64.99:5000/"
