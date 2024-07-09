bdefd1a4745795682f352ec5936a21a3212208c0
Adam29C <adamcatholic29@gmail.com>
2024-07-09 10:36:40 +0530

    change base url and add build

--- src/components/Config/config.js
+++ src/components/Config/config.js
@@ -1,4 +1,4 @@
 // export const base_url = "http://localhost:5000/"
 // export const base_url = "http://51.20.125.209:5000/"
 // export const base_url = "http://16.171.241.112:5000/"
-export const base_url = "http://13.51.64.99:5000/"
+export const base_url = "http://13.60.24.80:5000/"
